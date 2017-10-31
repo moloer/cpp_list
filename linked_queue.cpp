b9d4a11613029d5d07a9bd692f2072c11291c7fc
hello <790707389@qq.com>
2017-10-31 18:45:30 +0800

    c++链表

---
+++ linked_queue.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+class linked_link{
+	private:
+		char name[50];
+		linked_link *next;
+	public:
+		linked_link(linked_link *p_list,const char *del){
+				linked_link *p_newlist = p_list;
+				linked_link *Head = new ;
+				p_newlist = 
+				if(del == NULL)
+				{
+					p_newlist->name[0] = '\0';
+				}
+				else
+				{
+					strcpy(p_newlist->name, del);
+				}
+				p_newlist->next = NULL;
+		}
+};
+
+class queue{
+	private:
+		linked_link a;
+	public:
+
+		
+		
+		
+};
+
+int main(void)
+{
+}
+
+
+
